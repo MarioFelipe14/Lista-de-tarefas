fb11fe6a440c99192869a33cba23cfa9dbfafef6
MarioFelipe14 <mafebape140498@gmail.com>
2024-08-19 14:27:51 -0300

    atualizando js

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
-    document.querySelector('feito').addEventListener('click', function(){
+    document.querySelector('form').addEventListener('click', function(){
+    });
 
-    })
+    $('form').on('submit', function(e){
+        e.preventDefault(); 
+        let tarefa = $('input').val()
+        let lista = $("lista")
+        let item = `<li>${tarefa}</li>`
+        $(item).appendTo(lista)
+    });
 
     $('li').click(function() {
         $(this).addClass('feito');
     });
 
-    $('feito').on('submit', function(e){
-        e.preventDefault()
-    })
-})
+});
